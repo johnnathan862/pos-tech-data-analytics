4e839a3d5edec1c57204ff636a04527f64676647
Johnnathan Marques Lopes <johnnathan_lopes@hotmail.com>
2024-05-11 21:49:21 -0300

    Adding first version from streamliit app

---
+++ fase_4/tech_challenge/app.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+# Para rodar a aplicação é necessário rodar o comando "streamlit run app.py" no terminal dentro do venv
+
+# Adicionando um texto no streamlit
+st.write("# Previsão do valor do petróleo !")
